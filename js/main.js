d99a1d33ab204edf88b4950485247bf74f88f2d3
Andres <andrx_s@hotmail.com>
2017-04-21 18:15:18 -0500

    Slider

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+var pos = 0;
+var intv;
 $(document).on('ready',function(){
   init();
 });
@@ -10,14 +12,32 @@ function init() {
     $(e).css('width',width+'px');
     $(e).css('background-image',"url("+(url+".jpg")+")");
   });
+  // clearInterval(intv); -- A trabajar
+  intv = setInterval(handleClick,4000);
 }
 
 function handleClick() {
   var slide_target = 0;
   if ($(this).parent().hasClass('slider_controls')) {
     slide_target = $(this).index();
+    pos = slide_target;
+    clearInterval(intv);
+    intv = setInterval();
   }
+  else {
+    pos++;
+    if (pos>=$('.slide').length) {
+      pos = 0;
+    }
+    slide_target = pos;
+  }
+
+  // $('.slideContainer').animate({
+  //   'margin-left':-(slide_target * $('.slider_container').width())+'px'
+  // },'slow');
+
   // alert(-(slide_target * $('.slide').width()));
+
   $('.slideContainer').fadeOut('slow',function () {
     $(this).animate({
       'margin-left':-(slide_target * $('.slider_container').width())+'px'
